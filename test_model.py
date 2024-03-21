fb7df2f6882fbcb99860e8ab1cd98b0becbf5e6e
VergilX <abhinand.dmanoj@gmail.com>
2024-03-21 20:36:58 +0530

    init

---
+++ test_model.py
@@ -0,0 +1,11 @@
+import os
+import torch
+from torch.datasets import ImageFolder
+import torchvision.transforms as transforms
+
+
+
+# test_images = 
+
+# test_dir = "../input/new-plant-diseases-dataset/test"
+# test = ImageFolder(test_dir, transform=transforms.ToTensor())
