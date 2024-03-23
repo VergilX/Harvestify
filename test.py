a0765483cfa1c63f40a0fa8d2a19def89ac7d45a
VergilX <abhinand.dmanoj@gmail.com>
2024-03-23 20:34:31 +0530

    init

--- test.py
+++
@@ -1,7 +0,0 @@
-test_dir = "/home/tinkerspace/Abhinand/plant_dataset/dataset/test"
-test = ImageFolder(test_dir, transform=transforms.ToTensor())
-
-# predicting first image
-img, label = test[0]
-plt.imshow(img.permute(1, 2, 0))
-print('Label:', test_images[0], ', Predicted:', predict_image(img, model))
